131d6d2136bd4d7e82b4ff8b711fd570490c30f0
kazukiomori <kazuki1110170501@gmail.com>
2023-02-22 07:13:23 +0900

    Initial Commit

---
+++ simpleMoneyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleMoneyDiary
+//
+//  Created by Kazuki Omori on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
