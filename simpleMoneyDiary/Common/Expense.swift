40a2f6c36e114aa3196773e12361249f07b32744
kazukiomori <kazuki1110170501@gmail.com>
2023-03-04 09:45:16 +0900

    支出を記録するためのクラスを作成

---
+++ simpleMoneyDiary/Common/Expense.swift
@@ -0,0 +1,17 @@
+//
+//  Expense.swift
+//  simpleMoneyDiary
+//
+//  Created by Kazuki Omori on 2023/03/03.
+//
+
+import UIKit
+import RealmSwift
+
+@objcMembers
+class Expense: Object {
+    @objc dynamic var money: Int = 0
+    @objc dynamic var category: String = ""
+    @objc dynamic var date: Date = NSDate() as Date
+    @objc dynamic var memo: String = ""
+}
