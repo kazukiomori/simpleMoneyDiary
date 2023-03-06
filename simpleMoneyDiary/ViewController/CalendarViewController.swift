67feaa1468bff555624b3a74f45213a905ea2880
kazukiomori <kazuki1110170501@gmail.com>
2023-03-06 23:06:59 +0900

    FSCalendarのデリゲートを設定

--- simpleMoneyDiary/ViewController/CalendarViewController.swift
+++ simpleMoneyDiary/ViewController/CalendarViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 import FSCalendar
 import CalculateCalendarLogic
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    @IBOutlet weak var calendar: FSCalendar!
     
     // MARK: ライフサイクル
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendar.delegate = self
+        calendar.dataSource = self
     }
 }
